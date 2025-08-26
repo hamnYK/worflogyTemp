014b7cd79229a3843ecd67561d1dd722ee42def5
hamnYK <136779303+hamnYK@users.noreply.github.com>
2025-08-27 00:23:06 +0900

    update

---
+++ currentTime.js
@@ -0,0 +1,19 @@
+    function updateTime() {
+        const now = new Date();
+        const options = {
+            year: 'numeric',
+            month: 'long',
+            day: 'numeric',
+            weekday: 'long',
+            hour: '2-digit',
+            minute: '2-digit',
+            second: '2-digit',
+            hour12: false,
+            timeZone: 'Asia/Seoul'
+        };
+        const formatter = new Intl.DateTimeFormat('ko-KR', options);
+        document.getElementById('current-time').innerText = formatter.format(now);
+    }
+
+    setInterval(updateTime, 1000);
+    updateTime();
