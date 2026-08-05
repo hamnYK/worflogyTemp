d38d88481d3123c7cb6f20b76210934db8c688c6
worflogy <worflogy@gmail.com>
2026-08-05 17:59:49 +0900

    deploy: Auto-build Optimization Deployment (2026-08-05 17:59:49)

---
+++ js/company.js
@@ -0,0 +1,13 @@
+﻿document.addEventListener("DOMContentLoaded", () => {
+  new WorflogyModal({
+    openBtnId: "open-contact-modal",
+    closeBtnId: "close-ir-modal",
+    cancelBtnId: "cancel-ir-modal",
+    backdropId: "ir-modal-backdrop",
+    formId: "ir-proposal-form",
+    submitBtnId: "submit-ir-modal",
+    successScreenId: "ir-success-screen",
+    successCloseBtnId: "success-close-btn",
+    submitType: "inquiry"
+  });
+});
